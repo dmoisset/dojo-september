a5f13cd956f319ba3f9087fdf779deffec3a36c7
Daniel F Moisset <dmoisset@machinalis.com>
2016-09-08 21:07:54 +0100

    More grammar

--- knitting.py
+++ knitting.py
@@ -1,10 +1,12 @@
 from parsimonious import Grammar
 
 knit_grammar = Grammar(r"""
-    pattern = (row "\n")*
+    pattern = cast_on? (row "\n")*
+    cast_on = "CO" count "\n"
     row = stitch_group ("," stitch_group)*
-    stitch_group = stitch_kind count
-    stitch_kind = "p" / "k"
+    stitch_group = stitch_kind count stitch_scope?
+    stitch_kind = "p" / "k" / "sl st" / "yo"
+    stitch_scope = "tog" / "psso" / "pnso"
     count = ~"[0-9]"+
 """)
 
@@ -19,3 +21,5 @@ assert human_readable__pattern(example) == (
     knit 10, purl 10
     kint 15, purl 5
     """)
+
+# http://zderadicka.eu/writing-simple-parser-in-python/
